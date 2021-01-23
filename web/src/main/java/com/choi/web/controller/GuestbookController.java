b59be4056445c3d6c6bf4e689e81bfef945cafd6
choi <younggeunn@naver.com>
2021-01-23 11:44:42 +0900

    annotation 변경

--- web/src/main/java/com/choi/web/controller/GuestbookController.java
+++ web/src/main/java/com/choi/web/controller/GuestbookController.java
@@ -3,14 +3,14 @@ package com.choi.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.choi.web.domain.GuestbookVo;
 import com.choi.web.service.GuestbookService;
 
-@Controller
+@RestController
 public class GuestbookController {
 
 	@Autowired
