039fd873cc0f06adcdad286f7088bcb696939dbe
choi <younggeunn@naver.com>
2022-01-22 11:04:52 +0900

    - 바로가기 메뉴 추가

--- src/main/java/choi/web/springboot/controller/MainController.java
+++ src/main/java/choi/web/springboot/controller/MainController.java
@@ -10,6 +10,7 @@ import choi.web.springboot.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
@@ -55,6 +56,11 @@ public class MainController {
 
             return "main";
         }
+    }
+
+    @GetMapping("/shortcut")
+    public String shortcut() {
+        return "shortcut";
     }
 
 }
