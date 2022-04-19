2c65462844f2dbba09c6aa574a11920c8b58ab9f
choi <younggeunn@naver.com>
2022-04-19 21:32:16 +0900

    - 쿠키를 사용한 '아이디 저장하기' 기능 구현

--- src/main/java/choi/web/springboot/controller/MainController.java
+++ src/main/java/choi/web/springboot/controller/MainController.java
@@ -9,11 +9,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
@@ -28,12 +31,17 @@ public class MainController {
     private final MemberService memberService;
 
     @GetMapping("/")
-    public String signIn(Member member) {
+    public String signIn(@CookieValue(name = "rememberId", required = false) String rememberId, Member member, Model model) {
+        if (rememberId != null) {
+            member.setMemberEmail(rememberId);
+            model.addAttribute("isExistRememberId", true);
+        }
+
         return "main/login";
     }
 
     @PostMapping("/login")
-    public String login(Member member, Model model, HttpServletRequest request) {
+    public String login(Member member, Model model, HttpServletRequest request, HttpServletResponse response) {
         Member loginMember = memberService.findForLogin(member.getMemberEmail(), member.getMemberPassword());
         if (loginMember == null) {
             model.addAttribute("result", "아이디 또는 비밀번호를 확인해주세요.");
@@ -44,11 +52,23 @@ public class MainController {
                 model.addAttribute("result", "비활성화 계정입니다.");
 
                 return "main/login";
-            } else{
+            } else {
                 memberService.updateLastLoginDate(loginMember);
                 HttpSession session = request.getSession();
                 session.setAttribute("loginMember", loginMember);
                 log.info("Login Member = {}", loginMember.getMemberEmail());
+
+                if (request.getParameter("checkBox") == null) {
+                    Cookie cookie = new Cookie("rememberId", null);
+                    cookie.setMaxAge(0);
+                    cookie.setPath("/");
+                    response.addCookie(cookie);
+                } else {
+                    Cookie cookie = new Cookie("rememberId", String.valueOf(loginMember.getMemberEmail()));
+                    cookie.setMaxAge(60 * 60 * 24 * 30);
+                    response.addCookie(cookie);
+                }
+
 
                 return "redirect:/main";
             }
@@ -56,8 +76,14 @@ public class MainController {
     }
 
     @GetMapping("/logout")
-    public String logout(Member member, HttpSession session) {
+    public String logout(@CookieValue(name = "rememberId", required = false) String rememberId,
+                         Member member, HttpSession session, Model model) {
+        if (rememberId != null) {
+            member.setMemberEmail(rememberId);
+            model.addAttribute("isExistRememberId", true);
+        }
         session.invalidate();
+
         return "main/login";
     }
 
