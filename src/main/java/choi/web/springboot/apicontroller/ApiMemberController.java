192573f8362424d2effbe06fd7e67fc12ea844b5
choi <younggeunn@naver.com>
2022-05-09 21:48:57 +0900

    - 사용자 API 구현 : ID로 사용자 조회

--- src/main/java/choi/web/springboot/apicontroller/ApiMemberController.java
+++ src/main/java/choi/web/springboot/apicontroller/ApiMemberController.java
@@ -6,6 +6,7 @@ import choi.web.springboot.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -19,6 +20,11 @@ public class ApiMemberController {
     @GetMapping("/api/members")
     public @ResponseBody List<Member> findAll() {
         return memberService.findAll();
+    }
+
+    @GetMapping("/api/members/{memberId}")
+    public @ResponseBody Member findById(@PathVariable long memberId) {
+        return memberService.findByMemberId(memberId);
     }
 
 }
