91601db80e3c787d27d08cab3a6794b90de5e9a3
choi <younggeunn@naver.com>
2022-07-09 18:56:16 +0900

    :octocat: @Transactional 어노테이션으로 대체

--- src/test/java/choi/web/springboot/controller/SampleControllerTest.java
+++ src/test/java/choi/web/springboot/controller/SampleControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.support.DefaultTransactionDefinition;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,25 +20,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @ActiveProfiles(value = "mac")
+@Transactional
 class SampleControllerTest {
 
     @Autowired
     private MybatisService mybatisService;
 
-    @Autowired
-    private PlatformTransactionManager transactionManager;
+    /*
+        @Transactional
+            : @Transactional 어노테이션을 테스트 코드에 작성할 경우,
+              데이터를 롤백 시켜준다.
+              따라서, 아래의 롤백 코드없이 @Transactional 어노테이션만으로 기존 데이터를 유지할 수 있다.
+     */
 
-    TransactionStatus status;
-
-    @BeforeEach
-    void beforeEach() {
-        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-    }
-
-    @AfterEach
-    void afterEach() {
-        transactionManager.rollback(status);
-    }
+//    @Autowired
+//    private PlatformTransactionManager transactionManager;
+//
+//    TransactionStatus status;
+//
+//    @BeforeEach
+//    void beforeEach() {
+//        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+//    }
+//
+//    @AfterEach
+//    void afterEach() {
+//        transactionManager.rollback(status);
+//    }
 
     @Test
     void 등록() {
