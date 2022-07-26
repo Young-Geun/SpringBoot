2f04f7dc35fbe8fe85728ea146e3d9562436d1cf
choi <younggeunn@naver.com>
2022-07-26 15:28:19 +0900

    Aspect 우선순위 설정

--- src/main/java/choi/web/springboot/aop/TimeTraceAop.java
+++ src/main/java/choi/web/springboot/aop/TimeTraceAop.java
@@ -4,9 +4,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
+@Order(1)
 @Component
 @Slf4j
 public class TimeTraceAop {
