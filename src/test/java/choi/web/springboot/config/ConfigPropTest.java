91e9d192f8f1415c2040bfccfdfc1c7adaf2f9af
choi <younggeunn@naver.com>
2022-08-01 22:01:25 +0900

    properties에 대한 테스트 합침

--- src/test/java/choi/web/springboot/config/ConfigPropTest.java
+++ src/test/java/choi/web/springboot/config/ConfigPropTest.java
@@ -6,10 +6,17 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //@SpringBootTest
-@SpringBootTest(properties = "server.port = 8888")
+@SpringBootTest(
+        properties = {
+                "server.port = 8888",
+                "app.config.list-value = a,b,c"
+        }
+)
 @ActiveProfiles(value = "mac")
 class ConfigPropTest {
 
@@ -18,6 +25,12 @@ class ConfigPropTest {
 
     @Value("${server.port}")
     private String severPort;
+
+    @Value("${app.config.list-value}")
+    private String[] arrValue;
+
+    @Value("#{'${app.config.list-value}'.split(',')}")
+    private List<String> listValue;
 
     @Test
     void 설정값_가져오기() {
@@ -33,8 +46,22 @@ class ConfigPropTest {
 
     @Test
     void 재정의_값_가져오기() {
-        // @SpringBootTest(properties = "server.port = 8888") 으로 실행
+        // @SpringBootTest(properties = ...) 으로 실행
         assertEquals("8888", severPort);
+    }
+
+    @Test
+    void 배열로_가져오기() {
+        assertEquals("a", arrValue[0]);
+        assertEquals("b", arrValue[1]);
+        assertEquals("c", arrValue[2]);
+    }
+
+    @Test
+    void 리스트로_가져오기() {
+        assertEquals("a", listValue.get(0));
+        assertEquals("b", listValue.get(1));
+        assertEquals("c", listValue.get(2));
     }
 
 }
