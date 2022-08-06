9a2329d74d4cc1687beb879ed76ef7b448b6a8a8
choi <younggeunn@naver.com>
2022-08-06 20:22:42 +0900

    스프링배치 샘플코드

--- src/main/java/choi/web/springboot/SpringbootApplication.java
+++ src/main/java/choi/web/springboot/SpringbootApplication.java
@@ -1,10 +1,12 @@
 package choi.web.springboot;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableBatchProcessing
 @EnableScheduling
 @SpringBootApplication
 public class SpringbootApplication {
