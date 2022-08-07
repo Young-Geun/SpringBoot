8673f2869e9464ed6d7fe9bc074b98a231be9b0e
choi <younggeunn@naver.com>
2022-08-07 21:22:40 +0900

    멤버API 응답방식 변경

--- src/main/java/choi/web/springboot/apicontroller/ApiMemberController.java
+++ src/main/java/choi/web/springboot/apicontroller/ApiMemberController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 public class ApiMemberController {
@@ -17,24 +19,53 @@ public class ApiMemberController {
 
     @GetMapping("/api/members")
     public ResponseEntity findAll() {
-        return ResponseEntity.ok().body(memberService.findAll());
+        ResponseObject response = null;
+        try {
+            List<Member> findMembers = memberService.findAll();
+            if (findMembers == null) {
+                response = ResponseObject.builder()
+                        .responseCode("9999")
+                        .responseMsg("검색결과가 없습니다.")
+                        .build();
+            } else {
+                response = ResponseObject.builder()
+                        .responseCode("0000")
+                        .responseMsg("성공하였습니다.")
+                        .responseData(findMembers)
+                        .build();
+            }
+        } catch (Exception e) {
+            response = ResponseObject.builder()
+                    .responseCode("9999")
+                    .responseMsg("오류가 발생하였습니다.")
+                    .build();
+        }
+
+        return ResponseEntity.ok().body(response);
     }
 
     @GetMapping("/api/members/{memberId}")
     public ResponseEntity findById(@PathVariable long memberId) {
         ResponseObject response = null;
-        Member findMember = memberService.findByMemberId(memberId);
-        if (findMember == null) {
+        try {
+            Member findMember = memberService.findByMemberId(memberId);
+            if (findMember == null) {
+                response = ResponseObject.builder()
+                        .responseCode("9999")
+                        .responseMsg("검색결과가 없습니다.")
+                        .build();
+
+            } else {
+                response = ResponseObject.builder()
+                        .responseCode("0000")
+                        .responseMsg("성공하였습니다.")
+                        .responseData(findMember)
+                        .build();
+            }
+        } catch (Exception e) {
             response = ResponseObject.builder()
                     .responseCode("9999")
-                    .responseMsg("검색결과가 없습니다.")
-                    .build();
-
-        } else {
-            response = ResponseObject.builder()
-                    .responseCode("0000")
-                    .responseMsg("성공하였습니다.")
-                    .responseData(findMember)
+                    .responseMsg("오류가 발생하였습니다.")
                     .build();
         }
 
