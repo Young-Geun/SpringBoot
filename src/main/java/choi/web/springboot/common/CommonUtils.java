b6054be4a7e6a6712a67e0a37471745a3635ec6c
choi <younggeunn@naver.com>
2022-10-02 18:46:43 +0900

    사용자 테이블 컬럼추가 - 회원조회 페이지 수정사항 반영

--- src/main/java/choi/web/springboot/common/CommonUtils.java
+++ src/main/java/choi/web/springboot/common/CommonUtils.java
@@ -7,12 +7,35 @@ import java.text.DecimalFormat;
 @Component
 public class CommonUtils {
 
+    /**
+     * 숫자포맷 변경
+     *
+     * @param num
+     * @return
+     */
     public String numberFormat(Object num) {
         try {
             DecimalFormat df = new DecimalFormat("###,###.##");
             return df.format(num);
         } catch (Exception e) {
             return "0";
+        }
+    }
+
+    /**
+     * 날짜 포맷 변경
+     * Ex) dateStr = 20221002, format = "-"
+     * => 2022-10-02
+     *
+     * @param dateStr
+     * @param format
+     * @return
+     */
+    public String dateFormatYmd(String dateStr, String format) {
+        try {
+            return dateStr.substring(0, 4) + format + dateStr.substring(4, 6) + format + dateStr.substring(6, 8);
+        } catch (Exception e) {
+            return dateStr;
         }
     }
 
